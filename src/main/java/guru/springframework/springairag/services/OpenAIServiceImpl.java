f4f2946c707cc83bcb16ae00110fbffc77553703
PierreSQS <nomo_messina@web.de>
2025-03-14 21:21:09 +0100

    Sec5_Chap39: added code to use the vectorstore - step2

--- src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springairag/services/OpenAIServiceImpl.java
@@ -7,7 +7,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Pierrot, on 27-02-2025.
@@ -18,12 +26,26 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     private final ChatModel chatModel;
 
+    private final SimpleVectorStore simpleVectorStore;
+
+    @Value("classpath:template/rag-prompt-template.st")
+    private Resource ragPromptTemplate;
+
     @Override
     public Answer getAnswer(Question question) {
-        PromptTemplate promptTemplate = new PromptTemplate(question.question());
-        Prompt prompt = promptTemplate.create();
+        List<Document> documents = simpleVectorStore.similaritySearch(SearchRequest.builder()
+                .query(question.question()).topK(4).build());
+
+        assert documents != null;
+
+        List<String> documentContentList = documents.stream().map(Document::getText).toList();
+
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+        Prompt prompt = promptTemplate.create(Map.of("input", question.question(),
+                                                     "documents", String.join("\n", documentContentList)));
+
         ChatResponse response = chatModel.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getContent());
+        return new Answer(response.getResult().getOutput().getText());
     }
 }
