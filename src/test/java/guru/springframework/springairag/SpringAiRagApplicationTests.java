cebd3dc6d0fabb3762578df79b8896e57f170ac3
PierreSQS <nomo_messina@web.de>
2025-03-17 18:01:26 +0100

    Sec5_Chap42-XXX: implemented IT-Test with the LLM-Model used displayed

--- src/test/java/guru/springframework/springairag/SpringAiRagApplicationTests.java
+++ src/test/java/guru/springframework/springairag/SpringAiRagApplicationTests.java
@@ -1,13 +1,28 @@
 package guru.springframework.springairag;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.autoconfigure.openai.OpenAiEmbeddingProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringAiRagApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+
+        OpenAiEmbeddingProperties openAIEmbeddingProperties =
+                appCtx.getBean(OpenAiEmbeddingProperties.class);
+
+        String theModelInUse = openAIEmbeddingProperties.getOptions().getModel();
+
+        System.out.println("Beans in context: " + appCtx.getBeanDefinitionCount());
+        System.out.println("The model in use is: " + theModelInUse);
+
+        assertThat(theModelInUse).isEqualTo("text-embedding-3-small");
+
     }
 
 }
